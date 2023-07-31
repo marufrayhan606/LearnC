6774294ca798fbe4859f0713b3cd695c290e5cbc
Md. Maruf Rayhan <140908331+marufrayhan1623@users.noreply.github.com>
2023-07-31 21:35:15 +0600

    Add files via upload

---
+++ Practice Programs/Home_Projects/Chapter 2/Divisibility Check.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int x;
+        printf("Enter a Number: ");                  //Number Input
+        scanf("%d",&x);
+
+printf("%d",x%2==0);                                 //Result
+
+return 0;
+}
